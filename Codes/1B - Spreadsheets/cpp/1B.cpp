daa6f0578ca669487efcdece93c133a0463cc7ac
CodeByElie <nassarelie98@gmail.com>
2025-06-03 13:27:38 +0200

    1B

--- Codes/1B - Spreadsheets/cpp/1B.cpp
+++ Codes/1B - Spreadsheets/cpp/1B.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main() {
+
 
     int n;
     cin >> n;
@@ -10,20 +12,47 @@ int main() {
         string s;
         cin >> s;
         int type = 0;
+        int k = 0;
         if(s[0]=='R'){
             bool dig = false;
             for(int i=1;i<s.size();i++) {
                 if(isdigit(s[i])) dig=true;
                 if(!isdigit(s[i]) && dig) {
                     type = 1;
+                    k = i;
                 }
             }
         }
         if(type) {
-            cout << "type 1" <<endl;
+            int c = stoi(s.substr(k+1, s.size()));
+            string column = "";
+            while(c) {
+                int j = c/26;
+                int i = c-26*j;
+                if(i>0) {
+                    column = char(i+'A'-1)+column;
+
+                }else {
+                    column = 'Z' + column;
+                    j-=1;
+                }
+                c = j;
+            }
+            cout << column << s.substr(1, k-1) << endl;
         }else {
-            string firstPart="";
-            int k = i
+            string c = "";
+            int k= 0;
+            int column = 0;
+            for(int i=0;!isdigit(s[i]);i++) {
+                c+=s[i];
+                k++;
+            }
+            int power = 0;
+            for(int i=c.size()-1;i>=0;i--){
+                column+= (c[i]-'A'+1)*pow(26,power);
+                power++;
+            }
+            cout << "R" << s.substr(k, s.size()) << "C" << column << endl;
         }
         
     }
