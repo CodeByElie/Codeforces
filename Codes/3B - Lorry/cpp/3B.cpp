3c518ce3c887b29623f44efb75851eb54d8162e0
CodeByElie <nassarelie98@gmail.com>
2025-06-06 00:27:44 +0200

    3B

---
+++ Codes/3B - Lorry/cpp/3B.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+void display(vector<pair<int, int>> L)
+{
+    for (auto k : L)
+    {
+        cout << k.first << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n;
+    int t;
+    cin >> n >> t;
+
+    vector<pair<int, int>> kayaks;
+    vector<pair<int, int>> catamarans;
+
+    int tp, pp;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> tp >> pp;
+        if (tp == 1)
+            kayaks.push_back({pp, i + 1});
+        else
+            catamarans.push_back({pp, i + 1});
+    }
+
+    sort(kayaks.begin(), kayaks.end());
+    sort(catamarans.begin(), catamarans.end());
+
+    int m = 0;
+
+    int k1, k2, c, kcount;
+    vector<int> sol;
+
+    while (t > 1)
+    {
+        kcount = 0;
+        k1 = 0;
+        if (kayaks.size() >= 1)
+        {
+            k1 = kayaks.back().first;
+            kcount++;
+        }
+        k2 = 0;
+        if (kayaks.size() >= 2)
+        {
+            k2 = kayaks[kayaks.size() - 2].first;
+            kcount++;
+        }
+        c = 0;
+        if (catamarans.size() >= 1)
+            c = catamarans.back().first;
+
+        if (k1 + k2 == 0 and c == 0)
+            break;
+
+        if (k1 + k2 > c)
+        {
+            m += k1;
+            sol.push_back(kayaks.back().second);
+            kayaks.pop_back();
+            t -= 1;
+        }
+        else
+        {
+            m += c;
+            sol.push_back(catamarans.back().second);
+            catamarans.pop_back();
+            t -= 2;
+        }
+    }
+
+    if (t == 1 and kayaks.size() >= 1)
+    {
+        m += kayaks.back().first;
+        sol.push_back(kayaks.back().second);
+    }
+
+    cout << m << endl;
+    for (auto s : sol)
+    {
+        cout << s << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
