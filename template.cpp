64979ead661fa9c86725e8d6c42f8e7f0ce157a7
CodeByElie <nassarelie98@gmail.com>
2025-06-19 09:27:32 +0200

    Display Lists

--- template.cpp
+++ template.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 typedef long long ll;
 #define FR(a,b) for (int _ = a; _ < b; _++)
+#define DBG(L) for(auto e:L) cout << e << " "; cout << endl;
 
 int main() {
-
-
+    
     return 0;
 }
