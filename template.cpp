aca19f1838d5e352fe8f10287c1f80ebdeb64af9
CodeByElie <nassarelie98@gmail.com>
2025-06-20 09:58:33 +0200

    template

--- template.cpp
+++ template.cpp
@@ -3,10 +3,11 @@
 using namespace std;
 
 typedef long long ll;
-#define F(a,b) for (int _ = a; _ < b; _++)
+#define F(i,a,b) for (int i = a; i < b; i++)
 #define DL(L) for(auto e:L) cout << e << " "; cout << endl;
 
 int main() {
+
     
     return 0;
 }
