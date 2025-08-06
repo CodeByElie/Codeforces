d11a8c270df900af21e505e6f082e78fd5574e3b
Elie Nassar <nassarelie98@gmail.com>
2025-08-06 10:45:31 +0200

    Template

--- template.cpp
+++ template.cpp
@@ -3,12 +3,26 @@
 using namespace std;
 
 typedef long long ll;
-#define F(i,a,b) for (int i = a; i < b; i++)
-#define DL(L) for(auto e:L) cout << e << " "; cout << endl;
+#define F(i, a, b) for (ll i = a; i < b; i++)
+#define DL(L)             \
+    for (auto e : L)      \
+        cout << e << " "; \
+    cout << endl;
 
-int main() {
+
+void solve(int tc = 1) {
+    // cin >> tc;
+    while(tc--) {
+        //code
+    }
+}
+
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    solve();
 
     return 0;
 }
